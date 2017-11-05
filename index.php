2f8d2c8bc24b7a144001fdb1ec086bd199ff1d71
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-11-05 11:32:31 +0100

    stage for index.php view added to repo

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart Police</title>
+	<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<div style="margin-top: 300px;"></div>
+
+	<div class="container" align="center">
+		<div class="row">
+			<div class="col-md-6">
+				<p>Smart Police Projects</p>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
