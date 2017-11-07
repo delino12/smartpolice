e237628aced1600cdcb4442bc16d4e575221b00d
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-11-07 14:07:57 +0100

    added complaint tags with index

--- index.php
+++ index.php
@@ -50,6 +50,12 @@
     	<div class="row">
     		<div class="col-md-4 col-md-offset-2">
     			<h1 class="lead">Welcome to Smart Police Rescue</h1>
+    			<ul class="list-group">
+    				<li class="list-group-item">Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>
+    				<li class="list-group-item">Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>
+    				<li class="list-group-item">Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
+    				<li class="list-group-item">Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>
+    			</ul>
         	</div>
     	</div>
     </div>
