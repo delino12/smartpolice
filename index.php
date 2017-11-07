6864fd79e2709e0f58471cc52a95998f169dcdc2
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-11-07 23:16:23 +0100

    fix camera bug

--- index.php
+++ index.php
@@ -62,13 +62,38 @@
   {
   	if(isLoggedIn){
   		document.getElementById("dashboard").style.display = 'block';
-		document.getElementById("welcome-div").style.display = 'none';
+		  document.getElementById("welcome-div").style.display = 'none';
   		document.getElementById("fb-btn").style.display = 'none';
   		document.getElementById("logout").style.display = 'block';
 
+
+      // if only users is logged in
+      // Grab elements, create settings, etc.
+      var video = document.getElementById('video');
+
+      // Get access to the camera!
+      if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          // Not adding `{ audio: true }` since we only want video now
+          navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+              video.src = window.URL.createObjectURL(stream);
+              video.play();
+          });
+      }
+
+      // Elements for taking the snapshot
+      var canvas = document.getElementById('canvas');
+      var context = canvas.getContext('2d');
+      var video = document.getElementById('video');
+
+      // Trigger photo take
+      document.getElementById("snap").addEventListener("click", function() {
+        context.drawImage(video, 0, 0, 640, 480);
+      });
+
+
   	}else{
   		document.getElementById("dashboard").style.display = 'none';
-		document.getElementById("welcome-div").style.display = 'block';
+		  document.getElementById("welcome-div").style.display = 'block';
   		document.getElementById("fb-btn").style.display = 'block';
   		document.getElementById("logout").style.display = 'none';
   	}
@@ -99,28 +124,4 @@
     	</div>
     </div>
   </div> <!-- /container -->
-
-  	<script type="text/javascript">
-	  	// Grab elements, create settings, etc.
-		var video = document.getElementById('video');
-
-		// Get access to the camera!
-		if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-		    // Not adding `{ audio: true }` since we only want video now
-		    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-		        video.src = window.URL.createObjectURL(stream);
-		        video.play();
-		    });
-		}
-
-		// Elements for taking the snapshot
-		var canvas = document.getElementById('canvas');
-		var context = canvas.getContext('2d');
-		var video = document.getElementById('video');
-
-		// Trigger photo take
-		document.getElementById("snap").addEventListener("click", function() {
-			context.drawImage(video, 0, 0, 640, 480);
-		});
-  	</script>
 <?php include ('includes/foot-section.php'); ?>
