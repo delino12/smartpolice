a2b117eee4bc7ace6330173074aff6df08989f95
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-11-07 19:03:39 +0100

    added camera recorder

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include ('includes/head-section.php'); ?>
 <?php include ('includes/menu-section.php'); ?>
-  <br /><br /><br /><br />
+<br /><br /><br /><br />
 
 
 <script>
@@ -79,7 +79,10 @@
     	<div class="row">
     		<div class="col-md-4 col-md-offset-2">
     			<div id="dashboard">
-    				
+    				<h3>Capture live scene</h3>
+    				<video id="video" width="640" height="480" autoplay></video>
+					<button id="snap">Recored</button>
+					<canvas id="canvas" width="640" height="480"></canvas>
     			</div>
 
     			<div id="welcome-div">
@@ -96,4 +99,28 @@
     	</div>
     </div>
   </div> <!-- /container -->
+
+  	<script type="text/javascript">
+	  	// Grab elements, create settings, etc.
+		var video = document.getElementById('video');
+
+		// Get access to the camera!
+		if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+		    // Not adding `{ audio: true }` since we only want video now
+		    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+		        video.src = window.URL.createObjectURL(stream);
+		        video.play();
+		    });
+		}
+
+		// Elements for taking the snapshot
+		var canvas = document.getElementById('canvas');
+		var context = canvas.getContext('2d');
+		var video = document.getElementById('video');
+
+		// Trigger photo take
+		document.getElementById("snap").addEventListener("click", function() {
+			context.drawImage(video, 0, 0, 640, 480);
+		});
+  	</script>
 <?php include ('includes/foot-section.php'); ?>
