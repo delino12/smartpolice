219bc8de769ee09837d700df1ced7f7986a02802
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-11-09 00:15:59 +0100

    login bug fix

--- includes/menu-section.php
+++ includes/menu-section.php
@@ -1,28 +1,28 @@
 <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            </button>
-                <a class="navbar-brand" href="">Smart Police</a>
-            </div>
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="">Home</a></li>
-                    <li><a href="">Reports</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="#" id="fb-btn">
-                            <fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button> 
-                            or Signup via Facebook
-                        </a>
-                    </li>
-                    <li><a href="#" onclick="logoutUser()" id="logout">Logout</a></li>
-                </ul>
-            </div>
+    <div class="container">
+        <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        </button>
+            <a class="navbar-brand" href="">Smart Police</a>
         </div>
-    </nav>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="">Home</a></li>
+                <li><a href="">Reports</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#" id="fb-btn">
+                        <fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button> 
+                        or Signup via Facebook
+                    </a>
+                </li>
+                <li><a href="#" onclick="logoutUser()" id="logout">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
